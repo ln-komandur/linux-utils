deffed792d8f3d48d8135516d8fa2109b4c2dbdc
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2021-01-30 15:29:46 -0600

    Add files via upload

---
+++ installing jbig2enc - ubuntu20-04.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/agl/jbig2enc
+cd jbig2enc/
+sudo apt install build-essential automake autotools-dev libtool
+sudo apt install libleptonica-dev libjpeg8-dev libpng-dev libtiff5-dev zlib1g-dev
+./autogen.sh
+./configure 
+./configure && make
+sudo make install
