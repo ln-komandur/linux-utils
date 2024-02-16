b83daa368bbe5df13bac6687af7539eac1bfb446
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-02-16 11:37:46 -0600

    Works

--- zoom-updater.sh
+++ zoom-updater.sh
@@ -5,7 +5,7 @@
 installed_ver=$(cat /opt/zoom/version.txt)
 hosted_ver=$(wget --spider https://zoom.us/client/latest/zoom_amd64.deb 2>&1 | grep Location | sed -e 's/.*prod\/\(.*\)\/.*/\1/')
 echo Installed: $installed_ver
-echo Hosted: $hosted_ver 
+echo Hosted: $hosted_ver
 
 if [ $installed_ver = $hosted_ver ]; then
     echo "You have the latest version"
@@ -15,12 +15,10 @@ fi
 downloaded_file_name=zoom_amd64_$hosted_ver.deb
 
 response=Y
-echo -n "Download and install zoom version" $hosted_ver "over" $installed_ver "["$response"/n]?"
+echo -n "Download and install zoom version" $hosted_ver "over" $installed_ver "[Y/n]? "
 read response
-echo "Response is " $response
 
-
-if [ $response = Y ] || [ $response == y ] || [ $response = ""] ; then
+if [ $response = Y ] || [ $response == y ] || [ -z $response ] ; then
    wget -c https://zoom.us/client/latest/zoom_amd64.deb -O ./Downloads/$downloaded_file_name
    echo "###### Downloaded" $downloaded_file_name ". Installing now ######"
    sudo apt install ./Downloads/$downloaded_file_name
@@ -28,3 +26,4 @@ else
    echo "Retaining "$installed_ver". Exiting."
 fi
 exit
+
