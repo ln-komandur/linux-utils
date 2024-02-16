6f12fffc9eb4597b89760f51715d53957a8eacae
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-02-16 10:59:13 -0600

    mostly works. If condition needs improvement

--- zoom-updater.sh
+++ zoom-updater.sh
@@ -1,14 +1,30 @@
-#!/bin/sh
+  GNU nano 6.2                                                                                   zoom-updater.sh                                                                                            
+#!/bin/bash
+# Modified from https://community.zoom.com/t5/Meetings/Update-Zoom-in-Debian-Ubuntu/m-p/32820/highlight/true
 
-echo Installed: $(cat /opt/zoom/version.txt)
-echo Available: $(wget --spider https://zoom.us/client/latest/zoom_amd64.deb 2>>
+installed_ver=$(cat /opt/zoom/version.txt)
+hosted_ver=$(wget --spider https://zoom.us/client/latest/zoom_amd64.deb 2>&1 | grep Location | sed -e 's/.*prod\/\(.*\)\/.*/\1/')
+echo Installed: $installed_ver
+echo Hosted: $hosted_ver 
 
-REPLY=n
-echo -n "Download and install? "
-read REPLY
-if [ $REPLY = y ] ; then
-  wget -c https://zoom.us/client/latest/zoom_amd64.deb -P ./Downloads
-  sudo apt install ./Downloads/zoom_amd64.deb
-  rm ./Downloads/zoom_amd64.deb
+if [ $installed_ver = $hosted_ver ]; then
+    echo "You have the latest version"
+    exit 1;
+fi
+
+downloaded_file_name=zoom_amd64_$hosted_ver.deb
+
+response=Y
+echo -n "Download and install zoom version" $hosted_ver "over" $installed_ver "["$response"/n]?"
+read response
+echo "Response is " $response
+
+
+if [ $response = Y ] || [ $response == y ] || [ $response = ""] ; then
+   wget -c https://zoom.us/client/latest/zoom_amd64.deb -O ./Downloads/$downloaded_file_name
+   echo "###### Downloaded" $downloaded_file_name ". Installing now ######"
+   sudo apt install ./Downloads/$downloaded_file_name
+else
+   echo "Retaining "$installed_ver". Exiting."
 fi
 exit
