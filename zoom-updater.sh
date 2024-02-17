7e6695577df6da646c7673557dea698686963486
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-02-17 07:38:50 -0600

    improved messages

--- zoom-updater.sh
+++ zoom-updater.sh
@@ -3,34 +3,31 @@
 
 installed_ver=$(cat /opt/zoom/version.txt)
 hosted_ver=$(wget --spider https://zoom.us/client/latest/zoom_amd64.deb 2>&1 | grep Location | sed -e 's/.*prod\/\(.*\)\/.*/\1/')
-echo Installed version: $installed_ver
-echo Hosted version: $hosted_ver
 
 if [ "$installed_ver" = "$hosted_ver" ]; then
-    echo You have the latest version
+    echo The latest zoom version $installed_ver is already installed. Exiting.
     exit 1;
 fi
 
 deb_destn_path="./Downloads/"zoom_amd64_${hosted_ver//\./-}.deb #Replace all '.'s with '-'s in the version number
-
 response=Y
 
 if [ -z "$installed_ver" ] ; then
-    echo Downloading and installing zoom version $hosted_ver
+    echo -------- No installed version found. Downloading and installing zoom version $hosted_ver --------
 else
-    echo Download and install zoom version $hosted_ver over $installed_ver [Y/n]?
+    echo -n Download and install zoom version $hosted_ver over $installed_ver [Y/n]?
     read response
 fi
 
 if [ "$response" = Y ] || [ "$response" == y ] || [ -z "$response" ] ; then
     wget -c https://zoom.us/client/latest/zoom_amd64.deb -O $deb_destn_path
-    echo -------- Downloaded $deb_destn_path . Installing now --------
+    echo -------- Downloaded to $deb_destn_path . Installing now --------
 else
-    echo Retaining version: $installed_ver. Exiting.
+    echo -------- Retaining zoom version $installed_ver. Exiting. --------
     exit 1;
 fi
 
-if ! sudo nala install $deb_destn_path; then
+if ! sudo nala install $deb_destn_path; then # Use Apt if nala is not installed
      sudo apt install $deb_destn_path
 fi
 
