66ab6687ce863226b102a29c04f2ed87f5cfcb46
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-02-16 18:54:13 -0600

    File name, unary errors

--- zoom-updater.sh
+++ zoom-updater.sh
@@ -15,30 +15,27 @@ if (($EUID != 0)); then
   exit
 fi
 
-
 installed_ver=$(cat /opt/zoom/version.txt)
 hosted_ver=$(wget --spider https://zoom.us/client/latest/zoom_amd64.deb 2>&1 | grep Location | sed -e 's/.*prod\/\(.*\)\/.*/\1/')
-echo Installed: $installed_ver
-echo Hosted: $hosted_ver
+echo Installed version: $installed_ver
+echo Hosted version: $hosted_ver
 
-if [ $installed_ver = $hosted_ver ]; then
+if [ "$installed_ver" = "$hosted_ver" ]; then
     echo "You have the latest version"
     exit 1;
 fi
 
-deb_destn_path="./Downloads/"zoom_amd64_$hosted_ver.deb
+deb_destn_path="./Downloads/"zoom_amd64_${hosted_ver//\./-}.deb #Replace all '.'s with '-'s in the version number
 
 response=Y
 echo -n "Download and install zoom version" $hosted_ver "over" $installed_ver "[Y/n]?"
 read response
-echo "Response is " $response
 
-
-if [ $response = Y ] || [ $response == y ] || [ -z $response ] ; then
+if [ "$response" = Y ] || [ "$response" == y ] || [ -z "$response" ] ; then
    wget -c https://zoom.us/client/latest/zoom_amd64.deb -O $deb_destn_path
    echo "###### Downloaded" $deb_destn_path ". Installing now ######"
 else
-   echo "Retaining "$installed_ver". Exiting."
+   echo Retaining version: $installed_ver". Exiting."
    exit 1;
 fi
 
