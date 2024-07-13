6224b5d08c5228ee6d236bf7bcf3eadcb0b75d7b
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-07-13 08:24:50 -0500

    initial version

---
+++ install jupyter-notebook.sh
@@ -0,0 +1,15 @@
+# Per https://youtu.be/wotkLCoJ_ks
+
+./DiskSpaceJanitor.sh
+
+pip3 --version
+
+sudo pip3 install --upgrade pip
+
+pip3 --version
+
+sudo apt update && sudo apt upgrade -y
+
+sudo nala install jupyter-notebook 
+
+jupyter notebook 
