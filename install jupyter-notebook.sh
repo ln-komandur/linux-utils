63e8b26b0ed2aa20102b28626595760f8e91c488
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-07-13 09:48:27 -0500

    cleaned up to ask sudo privileges

--- install jupyter-notebook.sh
+++ install jupyter-notebook.sh
@@ -1,15 +1,48 @@
 # Per https://youtu.be/wotkLCoJ_ks
+#!/bin/bash
+#ref: https://unix.stackexchange.com/questions/28791/prompt-for-sudo-password-and-programmatically-elevate-privilege-in-bash-script
+#ref: https://askubuntu.com/a/30157/8698
 
-./DiskSpaceJanitor.sh
+if (($EUID != 0)); then
+  if [[ -t 1 ]]; then
+#https://unix.stackexchange.com/questions/218715/what-does-t-1-do
+    sudo "$0" "$@"
+  else
+    exec 1>output_file
+    gksu "$0 $@"
+  fi
+  exit
+fi
 
+echo "Current version of pip is..."
+echo "============================"
 pip3 --version
 
-sudo pip3 install --upgrade pip
 
+echo "Trying to upgrade pip."
+echo "======================"
+
+pip3 install --upgrade pip
+
+
+echo "pip version after trying to upgrade it."
+echo "======================================="
 pip3 --version
 
-sudo apt update && sudo apt upgrade -y
+echo "update and upgrade apt"
+echo "======================"
 
-sudo nala install jupyter-notebook 
+apt update && apt upgrade -y
 
-jupyter notebook 
+echo "-----------------------------------------"
+echo "Install jupyter-notebook with nala or apt"
+echo "-----------------------------------------"
+if ! nala install jupyter-notebook; then
+    apt install jupyter-notebook
+fi
+
+echo
+echo
+echo "Please use the command 'jupyter notebook' at the terminal to use jupyter notebook,"
+echo "or add its shortcut to ubuntu dock "
+echo "=================================================================================="
