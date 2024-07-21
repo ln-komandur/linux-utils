0957e270d1e56e86f8bd06c5d8f34405eb9062f0
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-07-21 10:36:56 -0500

    install command

--- install github-desktop-client.sh
+++ install github-desktop-client.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Instructions from https://apt.packages.shiftkey.dev/
-# Instructions also from https://github.com/shiftkey/desktop under @shiftkey package feed
+# Instructions for using APT package manager to install github-desktop and keep it up to date on Debian distributions.
+# from https://apt.packages.shiftkey.dev/ 
+# also also from https://github.com/shiftkey/desktop under @shiftkey package feed. 
 
-wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null # Get the key
+
+# Check if the user is su
 
 #ref: https://unix.stackexchange.com/questions/28791/prompt-for-sudo-password-and-programmatically-elevate-privilege-in-bash-script
 #ref: https://askubuntu.com/a/30157/8698
@@ -21,4 +24,6 @@ if (($EUID != 0)); then
 fi
 
 
-sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list' # Set-up the APT package feed
+apt update # Update apt
+apt install github-desktop # Install github-desktop
