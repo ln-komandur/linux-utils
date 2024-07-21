7452e2892eb25cb6141d2718bc9a06c3f228dc5d
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-07-21 10:28:37 -0500

    initial

---
+++ install github-desktop-client.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Instructions from https://apt.packages.shiftkey.dev/
+# Instructions also from https://github.com/shiftkey/desktop under @shiftkey package feed
+
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+
+
+#ref: https://unix.stackexchange.com/questions/28791/prompt-for-sudo-password-and-programmatically-elevate-privilege-in-bash-script
+#ref: https://askubuntu.com/a/30157/8698
+
+if (($EUID != 0)); then
+  if [[ -t 1 ]]; then
+#https://unix.stackexchange.com/questions/218715/what-does-t-1-do
+    sudo "$0" "$@"
+  else
+    exec 1>output_file
+    gksu "$0 $@"
+  fi
+  exit
+fi
+
+
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
