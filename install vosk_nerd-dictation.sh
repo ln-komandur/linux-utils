9500f7f074cce52fe461a1ff683159662a9d3de3
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-11-07 20:09:34 -0600

    Initial

---
+++ install vosk_nerd-dictation.sh
@@ -0,0 +1,39 @@
+echo
+echo pip3 install vosk #Install vosk for a regular user. This will place it under the user's $HOME
+pip3 install vosk #Install vosk for a regular user. This will place it under the user's $HOME
+
+echo
+echo . .profile #Set the PATH
+. .profile
+
+echo
+echo PATH
+echo $PATH
+
+echo
+echo cd .config/
+cd .config/
+
+echo
+echo git clone https://github.com/ideasman42/nerd-dictation.git
+git clone https://github.com/ideasman42/nerd-dictation.git
+
+echo
+echo cd nerd-dictation
+cd nerd-dictation
+
+echo
+echo wget https://alphacephei.com/kaldi/models/vosk-model-small-en-us-0.15.zip #Get the basic model
+wget https://alphacephei.com/kaldi/models/vosk-model-small-en-us-0.15.zip #Get the basic model
+
+echo
+echo unzip vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-en-us-0.15.zip
+
+echo
+echo mv vosk-model-small-en-us-0.15 model
+mv vosk-model-small-en-us-0.15 model
+
+echo
+echo ./nerd-dictation begin --vosk-model-dir=./model & #Run as a background process
+./nerd-dictation begin --vosk-model-dir=./model & #Run as a background process
---
+++ uninstall vosk_nerd-dictation.sh
@@ -0,0 +1,23 @@
+echo
+echo pip3 uninstall vosk srt #Uninstall vosk and srt. This will remove it from under the user's $HOME
+pip3 uninstall vosk srt #Uninstall vosk and srt. This will remove it from under the user's $HOME
+
+echo
+echo pip3 cache list
+pip3 cache list
+
+echo
+echo pip3 cache purge
+pip3 cache purge
+
+echo
+echo rm -rf ~/.cache/pip
+rm -rf ~/.cache/pip
+
+echo
+echo rm -rf ~/.config/nerd-dictation/ #Remove the nerd-dictation directory
+rm -rf ~/.config/nerd-dictation/ #Remove the nerd-dictation directory
+
+echo
+echo rm -rf .local/lib/python3.10/
+rm -rf .local/lib/python3.10/
