1548d2f8c50f22f75e430bcbba3b4022856a390c
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2025-06-14 07:09:01 -0500

    comments and validations

--- Extract_parts_of_video_and_combine_them.sh
+++ Extract_parts_of_video_and_combine_them.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+# Download the video using yt-dlp https://vimeo.com/1111111111
+
 if [ $# -le 1 ]
 then
     echo "Usage: Extract_parts_of_video_and_combine_them <Name of video file> <Name of prefix>"
     echo "Argument 1: <Name of the video file> from which to extract parts and combine"
     echo "Argument 2: <Name of prefix> to prepend the output video with"
+    echo "Example: "
+    echo "Extract_parts_of_video_and_combine_them all_performances.mp4 Singing_in_"
     echo "Exiting"
     exit 1
 fi
 part_number=0 # Start with 0 as there are no parts to assemble yet
 > $2_list_of_video_parts_to_concat.txt # Create an empty file to list the file names of all parts
+
+echo "------------------------------------------------------------"
+echo
+echo "To extract part "$part_number" of the video from" $1 ", enter Start time and Duration as hh:mm:ss hh:mm:ss"
+
 while true; do
 	part_number=$((part_number+1)) # Increment the part number
 	while
-		echo "------------------------------------------------------------"
-		echo
-	  	echo "To extract part "$part_number" of the video from" $1 ", enter Start time and Duration as hh:mm:ss hh:mm:ss"
-	  	read start_time duration # Get the user input
-	  	[[ -z $start_time || $start_time == *[^0-9:]* ]] # If an empty string or containing anything other than "0-9 / :" 
-	  	[[ -z $duration || $duration == *[^0-9:]* ]] # If an empty string or containing anything other than "0-9 / :"
+		echo "Example: To start at 2 minutes and to last 14 seconds, enter"
+		echo "00:02:00 00:00:14"
+		read -p ">" start_time duration # Get the user input
+	  	[[ -z $start_time || $start_time == *[^0-9:]* || -z $duration || $duration == *[^0-9:]* ]] # Prompt again if an empty string or containing anything other than "0-9 / :"
 	do echo "Please enter a valid start time and duration in hh:mm:ss hh:mm:ss"; done
 
 	ext="${1##*.}" # The single # operator is used to try and remove the shortest text matching the pattern, while ## tries to do it with the longest text matching from the beginning of the string.
@@ -40,7 +47,7 @@ while true; do
     	case $yn in
     		[Yy]* ) echo "Great. Let's move on.";; #These options are needed in the case statement to exhaust all valid options. break should not be included as it will break from the loop
 		[Nn]* ) echo "Combining parts now";
-			ffmpeg -f concat -safe 0 -i $2_list_of_video_parts_to_concat.txt -c copy "$2_$1"; #Concatenate all video parts
+			ffmpeg -f concat -safe 0 -i $2_list_of_video_parts_to_concat.txt -c copy "$2_$1"; #Concatenate all video parts. Use -safe 0 to accommodate white space in file names
 		        break;; # Break from the loop
 		* ) echo "Please answer Y or N.";;
 	esac
